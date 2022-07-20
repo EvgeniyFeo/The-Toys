a6dcce9a2579f67afc61a7d1a27e7ebc538b0325
Evgeniy Feoktistov <evgene.feoktistov@gmail.com>
2022-07-20 20:17:10 +0300

    Initial Commit

---
+++ The Toys/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Toys
+//
+//  Created by Eugene Feoktistov on 7/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
